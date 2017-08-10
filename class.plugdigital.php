98e142b0dd5cf13440cc7a2f54b088b5a20cbe66
Manuel Padilla <manuelpadilla@Manuels-iMac.local>
2017-08-10 15:23:41 -0500

    Adding Google Analytics ID to the front-end and project restructuration

--- class.plugdigital.php
+++ class.plugdigital.php
@@ -3,12 +3,54 @@
 * pdmx - PlugDigital México
 */
 class pdmx {
-	const NONCE = 'plugdigital';
+	const NONCE               = 'plugdigital';
+	private static $initiated = false;
 
 	public static function init() {
 		if ( isset( $_POST['action'] ) && $_POST['action'] == 'enter-pdmx-google-analytics-id' ) {
 			self::enter_pdmx_google_analytics_id();
 		}
+
+		if ( ! self::$initiated ) {
+			self::init_hooks();
+		}
+	}
+
+	/**
+	 * Initializes WordPress hooks
+	 */
+	private static function init_hooks() {
+		self::$initiated = true;
+
+		add_action( 'wp_before_admin_bar_render', array( 'pdmx', 'pdmx_add_our_link' ) );
+		add_action( 'login_footer', array( 'pdmx', 'pdmx_login_powered' ) );
+		add_action( 'admin_menu', array( 'pdmx', 'pdmx_settings_menu' ) );
+		add_action( 'wp_head', array( 'pdmx', 'pdmx_google_analytics_code' ) );
+	}
+
+	/**
+	 * Add Plug Digital link at top bar
+	 */
+	public static function pdmx_add_our_link(){
+		global $wp_admin_bar;
+
+		$wp_admin_bar->add_menu( array(
+			'id'    => 'plugdigital-site',
+			'title' => 'Plug Digital',
+			'href'  => 'https://www.plugdigital.net'
+		) );
+	}
+
+	/**
+	 * Add Powered By Plug Digital at login form
+	 */
+	public static function pdmx_login_powered() {
+		pdmx::view('powered-by');
+	}
+
+
+	public static function pdmx_start_page() {
+		pdmx::view('start');
 	}
 
 	public static function view( $name, array $args = array() ) {
@@ -27,7 +69,7 @@ class pdmx {
 
 	// Add options page
 	public static function pdmx_settings_menu() {
-		add_options_page( 'Optimización y Rendimiento', 'Optimización y Rendimiento', 'manage_options', 'pdmx-settings-menu', 'pdmx_start_page' );
+		add_options_page( 'Optimización y Rendimiento', 'Optimización y Rendimiento', 'manage_options', 'pdmx-settings-menu', 'pdmx::pdmx_start_page' );
 	}
 
 
@@ -39,38 +81,26 @@ class pdmx {
 
 		return true;
 	}
+
+	/**
+	 * Add Google Analytics ID to the head
+	 */
+	public static function pdmx_google_analytics_code( ) {
+		if ( get_option( 'pdmx_google_analytics_id' ) ): ?>
+			<!-- Google Analytics -->
+			<script>
+				(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+				(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+				m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+				})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+				ga('create', '<?php echo get_option( 'pdmx_google_analytics_id' ); ?>', 'auto');
+				ga('send', 'pageview');
+			</script>
+			<!-- END Google Analytics -->
+		<?php endif;
+	}
 } // class pdmx end
-
-/**
- * Add Plug Digital link at top bar
- */
-function pdmx_add_our_link(){
-	global $wp_admin_bar;
-
-	$wp_admin_bar->add_menu( array(
-		'id'    => 'plugdigital-site',
-		'title' => 'Plug Digital',
-		'href'  => 'https://www.plugdigital.net'
-	) );
-}
-add_action( 'wp_before_admin_bar_render', 'pdmx_add_our_link' );
-
-
-/**
- * Add Powered By Plug Digital at login form
- */
-function pdmx_login_powered() {
-	pdmx::view('powered-by');
-}
-add_action( 'login_footer', 'pdmx_login_powered' );
-
-function pdmx_start_page() {
-	pdmx::view('start');
-}
-add_action( 'admin_menu', 'pdmx::pdmx_settings_menu' );
-
-
-
 
 
 
